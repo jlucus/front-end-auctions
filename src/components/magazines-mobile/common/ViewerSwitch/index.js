6595916dcbb023f844c57145a6ca66addc5e02a8
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-05-12 20:23:05 +0000

    fix mobile view

--- src/components/magazines-mobile/common/ViewerSwitch/index.js
+++ src/components/magazines-mobile/common/ViewerSwitch/index.js
@@ -10,15 +10,10 @@ const viewerList = [
     id: 'magazineview',
     caption: 'Magazine View'
   },
-  {
-    id: 'webview',
-    caption: 'Web View'
-  }
 ]
 
 const ViewerSwitch = props => {
   const { viewers, onSwitchViewer, mapSwitch } = props
-  const width = window.innerWidth
   const selectedViewers = viewerList.filter(
     item => 
       viewers.findIndex(
@@ -34,18 +29,16 @@ const ViewerSwitch = props => {
     >
       {
         selectedViewers.map((item, index) => {
-          if(!(item.id === 'webview' && width < 768)) {
-            return (
-              <button
-                key={index}
-                onClick={() => onSwitchViewer(item.id)}
-              >
-                {
-                  item.caption
-                }
-              </button>
-            )
-          }
+          return (
+            <button
+              key={index}
+              onClick={() => onSwitchViewer(item.id)}
+            >
+              {
+                item.caption
+              }
+            </button>
+          )
         })
       }
     </div>
--- src/pages/magazines/[id]/index.js
+++ src/pages/magazines/[id]/index.js
@@ -2,52 +2,60 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import WebViewer from '../../../containers/web-view'
 import MagazineViewer from '../../../containers/magazine-view'
+import MagazineMobile from '../../../containers/magazine-mobile'
 import MapViewer from '../../../containers/map-view'
 
-const MagazinePages = () => {
+const MagazinePages = props => {
   const router = useRouter()
   const { id } = router.query;
 
-  const [viewMethod, setViewMethod] = useState('magazineview')
-  const [currentPage, setCurrentPage] = useState(0)
+  const [viewMethod, setViewMethod] = useState('mapview')
+  const width = window.innerWidth
   
   if (viewMethod === 'webview') {
     return (
       <WebViewer
         issueId={id}
-        initPage={currentPage}
         onSwitchViewer={
           viewer => {
             setViewMethod(viewer)
-          }
-        }
-        onChangePageNumber={
-          number => {
-            setCurrentPage(number)
           }
         }
       >
       </WebViewer>
     )
   } else if (viewMethod === 'magazineview') {
-    return (
-      <MagazineViewer
-        issueId={id}
-        onSwitchViewer={
-          viewer => {
-            setViewMethod(viewer)
+    if(width > 768) {
+      return (
+        <MagazineViewer
+          issueId={id}
+          onSwitchViewer={
+            viewer => {
+              setViewMethod(viewer)
+            }
           }
-        }
-      >
-      </MagazineViewer>
-    )
+        >
+        </MagazineViewer>
+      )
+    }else {
+      return (
+        <MagazineMobile
+          issueId={id}
+          onSwitchViewer={
+            viewer => {
+              setViewMethod(viewer)
+            }
+          }
+        >
+        </MagazineMobile>
+      )
+    }
   }
 
   return (
     <MapViewer
       issueId={id}
       onClickItem={pageNumber => {
-        setCurrentPage(pageNumber)
         setViewMethod('webview')
       }}
       onSwitchViewer={
